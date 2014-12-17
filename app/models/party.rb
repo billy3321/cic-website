5b1b9a75d47854756aa0406ecd79459209421783
billy3321 <billy3321@gmail.com>
2014-12-17 18:40:49 +0800

    change syntax

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ActiveRecord::Base
   has_many :elections
   has_many :legislators, through: :elections
-  has_many :ads, -> { uniq }, :through => :elections
+  has_many :ads, -> { uniq }, through: :elections
 end
