e44d1b6947a83db07a087c0a18cfa02bd60d75bb
billy3321 <billy3321@gmail.com>
2014-12-17 16:59:09 +0800

    fix out of date error

--- spec/factories/ad.rb
+++ spec/factories/ad.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :ad do
     sequence(:name)  { |n| "Ad #{n}" }
-    sequence(:vote_date, 1) { |n| Date.today - ( 4 * ( n )).years }
-    sequence(:term_start, 1) { |n| Date.today - ( 4 * ( n )).years }
-    sequence(:term_end, 1) { |n| Date.today - ( 4 * ( n - 1 )).years }
+    sequence(:vote_date, 1) { |n| Date.today - ( 4 * ( (1..10).to_a[n % 10] )).years }
+    sequence(:term_start, 1) { |n| Date.today - ( 4 * ( (1..10).to_a[n % 10] )).years }
+    sequence(:term_end, 1) { |n| Date.today - ( 4 * ( (1..10).to_a[n % 10] )).years }
   end
 end
--- spec/factories/ad_sessions.rb
+++ spec/factories/ad_sessions.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :ad_session do
     sequence(:name)  { |n| "AdSession #{n}" }
-    sequence(:date_start, 1) { |n| Date.today - ( 6 * ( n )).months }
-    sequence(:date_end, 1) { |n| Date.today - ( 6 * ( n - 1 )).months }
+    sequence(:date_start, 1) { |n| Date.today - ( 6 * ( (1..10).to_a[n % 10] )).months }
+    sequence(:date_end, 1) { |n| Date.today - ( 6 * ( (1..10).to_a[n % 10] - 1 )).months }
     ad { FactoryGirl.create(:ad) }
   end
 end
