2d56c599090a43ff01efc8cf2ebcd2b013d4eb07
billy3321 <billy3321@gmail.com>
2014-12-30 23:56:39 +0800

    add some test

--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -6,15 +6,35 @@ describe "Static pages" do
   let(:admin) { FactoryGirl.create(:admin) }
 
   describe "before login" do
-    describe "#home" do
+    describe "#home with nothing" do
       it "success" do
         get "/"
         response.should be_success
       end
     end
 
+    describe "#home" do
+      it "success" do
+        3.times { FactoryGirl.create :entry }
+        3.times { FactoryGirl.create :question }
+        3.times { FactoryGirl.create :video_news }
+        get "/"
+        response.should be_success
+      end
+    end
+
+    describe "#recent with nothing" do
+      it "success" do
+        get "/recent"
+        response.should be_success
+      end
+    end
+
     describe "#recent" do
       it "success" do
+        3.times { FactoryGirl.create :entry }
+        3.times { FactoryGirl.create :question }
+        3.times { FactoryGirl.create :video_news }
         get "/recent"
         response.should be_success
       end
