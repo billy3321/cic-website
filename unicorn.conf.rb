3ebdc401392a22dfa237edfbcfc32df5e171bd5f
billy3321 <billy3321@gmail.com>
2015-01-23 10:59:13 +0800

    fix unicorn config

--- unicorn.conf.rb
+++ unicorn.conf.rb
@@ -1,5 +1,5 @@
 working_directory ""
-listen "/var/run/unicorn.sock", :backlog => 64
-pid "/var/run/unicorn.pid"
+listen "/var/run/unicorn/unicorn.sock", :backlog => 64
+pid "/var/run/unicorn/unicorn.pid"
 stderr_path "/var/log/unicorn.stderr.log"
 stdout_path "/var/log/unicorn.stdout.log"
