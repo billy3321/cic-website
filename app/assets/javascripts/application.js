ba1c7e8efae0ab47b5eb4276a8461410d615576f
billy3321 <billy3321@gmail.com>
2015-02-28 02:05:18 +0800

    fix disqus js error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -244,13 +244,14 @@ var ready = function(){
     }
   });
 
-  var disqus_shortname = 'cic-tw';
-
-  (function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-  })();
+  if ( $("#disqus_thread").length ){
+    var disqus_shortname = 'cic-tw';
+    (function() {
+      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+  }
 
 };
 
