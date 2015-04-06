3420997367250b46cdb5429dc971c61a18b913c8
billy3321 <billy3321@gmail.com>
2015-04-06 22:48:58 +0800

    add short_name function

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -7,4 +7,14 @@ class Committee < ActiveRecord::Base
   has_many :ccw_citizen_scores
   has_many :ccw_legislator_data, through: :legislator_committees
   validates_presence_of :name,  message: '請填寫委員會名稱'
+
+  def short_name
+    if self.name == "社會福利及衛生環境委員會"
+      "社福及衛環"
+    elsif self.name == "院會"
+      self.name
+    else
+      self.name.gsub(/委員會/, '')
+    end
+  end
 end
