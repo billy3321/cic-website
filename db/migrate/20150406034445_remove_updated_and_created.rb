9552f8bf1adde9774a91bc084ad531674b924f54
billy3321 <billy3321@gmail.com>
2015-04-06 15:10:20 +0800

    modify the column type

--- db/migrate/20150406034445_remove_updated_and_created.rb
+++ db/migrate/20150406034445_remove_updated_and_created.rb
@@ -1,16 +1,16 @@
 class RemoveUpdatedAndCreated < ActiveRecord::Migration
   def change
-    remove_column :ad_sessions, :created_at, :date
-    remove_column :ad_sessions, :updated_at, :date
-    remove_column :ads, :created_at, :date
-    remove_column :ads, :updated_at, :date
-    remove_column :committees, :created_at, :date
-    remove_column :committees, :updated_at, :date
-    remove_column :elections, :created_at, :date
-    remove_column :elections, :updated_at, :date
-    remove_column :legislators, :created_at, :date
-    remove_column :legislators, :updated_at, :date
-    remove_column :parties, :created_at, :date
-    remove_column :parties, :updated_at, :date
+    remove_column :ad_sessions, :created_at, :datetime
+    remove_column :ad_sessions, :updated_at, :datetime
+    remove_column :ads, :created_at, :datetime
+    remove_column :ads, :updated_at, :datetime
+    remove_column :committees, :created_at, :datetime
+    remove_column :committees, :updated_at, :datetime
+    remove_column :elections, :created_at, :datetime
+    remove_column :elections, :updated_at, :datetime
+    remove_column :legislators, :created_at, :datetime
+    remove_column :legislators, :updated_at, :datetime
+    remove_column :parties, :created_at, :datetime
+    remove_column :parties, :updated_at, :datetime
   end
 end
