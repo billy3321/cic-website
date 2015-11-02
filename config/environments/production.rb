2ae77ddff12908c3f4c525f4fa7d71a28fc0b5cb
billy3321 <billy3321@gmail.com>
2015-11-02 17:06:27 +0800

    add ip setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   config.lograge.enabled = true
   config.lograge.custom_options = lambda do |event|
-    options = event.payload.slice(:request_id, :user_id, :visit_id)
+    options = event.payload.slice(:request_id, :user_id, :ip)
     options[:params] = event.payload[:params].except("controller", "action")
     options
   end
