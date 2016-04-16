3edd8b210aad611cda7b91abaec5d13b0887daf4
billy3321 <billy3321@gmail.com>
2016-04-16 12:06:15 +0800

    add meta tags

--- app/controllers/events/incantations_controller.rb
+++ app/controllers/events/incantations_controller.rb
@@ -4,12 +4,50 @@ class Events::IncantationsController < ApplicationController
   before_action :set_legislators, only: [:show]
 
   def show
+    set_meta_tags({
+      title: "#{@incantation.title}-天眼公民咒",
+      description: "天眼公民咒，#{@incantation.title}，你要為誰貼咒？",
+      keywords: "#{@incantation.title},天眼公民咒,公民廟口,國會調查兵團",
+      og: {
+        type: 'website',
+        title: "#{@incantation.title}-天眼公民咒",
+        description: "天眼公民咒，#{@incantation.title}，你要為誰貼咒？",
+        image: "/images/events/incantations/inc-0#{@incantation.id}-fb.gif",
+        site_name: "國會調查兵團"
+      },
+      article: {
+        author: "https://www.facebook.com/cictw/",
+        publisher: "https://www.facebook.com/cictw/"
+      },
+      twitter: {
+        image: "/images/events/incantations/inc-0#{@incantation.id}-fb.gif"
+      }
+    })
   end
 
   def result
     if @legislator == nil or @legislator.elections.last.ad.id != 9
       redirect_to events_incantation_path(@incantation)
     end
+    set_meta_tags({
+      title: "#{@legislator.name}-#{@incantation.title}",
+      description: "#{@legislator.name}聽我令：#{@incantation.word}",
+      keywords: @legislator.name,
+      og: {
+        type: 'profile',
+        title: "#{@legislator.name}-#{@incantation.title}",
+        description: "#{@legislator.name}聽我令：#{@incantation.word}",
+        image: "/images/events/incantations/inc-0#{@incantation.id}-fb.gif",
+        site_name: "國會調查兵團"
+      },
+      article: {
+        author: "https://www.facebook.com/cictw/",
+        publisher: "https://www.facebook.com/cictw/"
+      },
+      twitter: {
+        image: "/images/events/incantations/inc-0#{@incantation.id}-fb.gif"
+      }
+    })
   end
 
   private
