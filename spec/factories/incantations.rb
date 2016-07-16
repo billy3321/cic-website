dc8c7c7a64db1adeb47f90d6bc1041493f2c6bcd
billy3321 <billy3321@gmail.com>
2016-07-16 18:28:22 +0800

    add incantation test

--- spec/factories/incantations.rb
+++ spec/factories/incantations.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :incantation do
-    
+    sequence(:title)  { |n| "Incantation #{n}" }
+    sequence(:word) { |n| "Incantation_#{n} Word"}
+    positive true
   end
-
 end
--- spec/models/incantation_spec.rb
+++ spec/models/incantation_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Incantation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Incantation do
+  it "#factory_creat_success" do
+    expect {
+      FactoryGirl.create :incantation
+    }.to change { Incantation.count }.by(1)
+  end
 end
